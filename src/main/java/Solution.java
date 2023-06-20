43c28cc435593c58488f2f5aa497048fa8cbb918
alek <wait82985@gmail.com>
2023-06-20 18:34:39 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,22 @@
 public class Solution {
     public int jump(int[] nums) {
-        //TODO
-        return 0;
+        Integer[] memo = new Integer[nums.length + 1];
+        return dfs(nums, 0, memo);
+    }
+
+    public int dfs(int[] nums, int i, Integer[] memo) {
+        if (i >= nums.length - 1) {
+            return 0;
+        }
+        if (memo[i] != null) {
+            return memo[i];
+        }
+
+        int cost = Integer.MAX_VALUE / 2;
+        for (int j = 1; j <= nums[i]; j++) {
+            cost = Math.min(cost, 1 + dfs(nums, i + j, memo));
+        }
+        memo[i] = cost;
+        return cost;
     }
 }
